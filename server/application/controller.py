2cc3117130ec484f057a72055dd0b3da0b7941cc
krian <leonid.soputnyak@gmail.com>
2023-07-17 11:46:26 +0300

    Add type hints

--- server/application/controller.py
+++ server/application/controller.py
@@ -1,5 +1,7 @@
-from typing import Dict, Union
-
+from typing import (
+    Dict,
+    Union
+)
 from server.application.strategy.calculate_strategy import \
     StrategyContext as CalculateStrategyContext
 
